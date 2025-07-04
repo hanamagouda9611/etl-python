9c23c1951b4dff97a62d7ebe562160de82ef34fe
hanamagouda9611 <hanamagoudpatil9611@gmail.com>
2025-07-04 12:08:16 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# STEP 1: EXTRACT
+csv_path = r"d:\DOCUMENT\COE_Associate_Details.xlsx"
+
+df = pd.read_excel(csv_path)
+
+print("✅ Data preview:")
+print(df.head())
+print("✅ Raw length:", len(df))
+
+# STEP 2: TRANSFORM
+df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
+
+# Only drop rows where all cells are blank
+df = df.dropna(how="all")
+
+print("✅ Length after dropna(how='all'):", len(df))
+
+# STEP 3: LOAD
+engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:7051/postgres")
+
+df.to_sql("my_table", con=engine, if_exists="replace", index=False)
+
+print("✅ ETL pipeline completed successfully!")
